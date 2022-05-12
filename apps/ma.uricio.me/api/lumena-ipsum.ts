c8980c2ed1b8978f603aa04fffb82079aee16450
Mauricio Carnieletto <mauricio.carnieletto@gmail.com>
2022-05-12 00:31:02 -0300

    Removing unused and polemic api methods

--- apps/ma.uricio.me/api/lumena-ipsum.ts
+++
@@ -1,6 +0,0 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
-import { data } from "./lumena-data";
-
-export default (_req: VercelRequest, res: VercelResponse) => {
-  res.status(200).send(data);
-};
